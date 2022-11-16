de5872e7f0728e22729a7d825d3dec232b5e0f3f
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-11-16 21:50:14 +0100

    Added automatic db seeding on project startup

--- src/Movies.API/Program.cs
+++ src/Movies.API/Program.cs
@@ -16,6 +16,14 @@ builder.Services.AddDbContext<ApplicationContext>(opt =>
 
 var app = builder.Build();
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationContext>();
+    MoviesContextSeed.SeedAsync(context);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {

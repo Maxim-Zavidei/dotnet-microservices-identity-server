46fde2af65dc5abcca4d9a99f8d7eb87128e192b
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-11-17 20:33:46 +0100

    Initial creation of the client mvc application

---
+++ src/Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,31 @@
+using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices;
+
+public class MovieApiService : IMovieApiService
+{
+    public async Task<Movie> CreateMovie(Movie movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteMovie(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Movie> GetMovie(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Movie>> GetMovies()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Movie> UpdateMovie(Movie movie)
+    {
+        throw new NotImplementedException();
+    }
+}

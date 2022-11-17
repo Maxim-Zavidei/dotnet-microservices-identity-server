96da6a266f31abbf6dc8e89a891b553ea4e07077
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-11-17 18:09:25 +0100

    Setup identity server

--- src/IdentityServer/Program.cs
+++ src/IdentityServer/Program.cs
@@ -1,5 +1,22 @@
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(new List<Client>())
+    .AddInMemoryIdentityResources(new List<IdentityResource>())
+    .AddInMemoryApiResources(new List<ApiResource>())
+    .AddInMemoryApiScopes(new List<ApiScope>())
+    .AddTestUsers(new List<TestUser>())
+    .AddDeveloperSigningCredential();
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+app.UseIdentityServer();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 

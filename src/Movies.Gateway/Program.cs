719f04cca21b95d6bba904bec8101a1472570dfd
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-11-27 18:10:20 +0100

    Added authentication to gateway

--- src/Movies.Gateway/Program.cs
+++ src/Movies.Gateway/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var authenticationProviderKey = "IdentityApiKey";
+
+builder.Services.AddAuthentication().AddJwtBearer(authenticationProviderKey, x =>
+{
+    x.Authority = "https://localhost:5005";
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false
+    };
+});
 
 builder.Configuration.AddJsonFile("ocelot.json");
 builder.Services.AddOcelot(builder.Configuration);
